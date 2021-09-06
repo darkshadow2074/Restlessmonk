efc44ced78f8f4d27dc769c3f484fe3f8658a220
darkshadow2074 <akashpre196@gmail.com>
2021-09-06 17:24:33 +0530

    App.js : Update by adding new component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import AccountSection from './components/AccountSection';
+import Card from './components/Card';
+import HeadSection from './components/HeadSection';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <HeadSection />
+     <Card/>
+     <AccountSection/>
     </div>
   );
 }
